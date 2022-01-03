ac07fdce2f0bc1f7d5839f6a851a98bbc8301424
André Lacerda <andrlacerda180@gmail.com>
2022-01-03 09:23:02 -0300

    separate slices into store folder in files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,10 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { counterActions } from '../store'
+import { counterActions } from '../store/counter'
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch()
-  const counter = useSelector(state => state.counter.value)
+  const counter = useSelector(state => state.counter.counter)
   const show = useSelector(state => state.counter.showCounter)
 
   const incrementHandler = () => {
